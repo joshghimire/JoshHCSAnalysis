4624a28518af1b5197112f773ae48eb57b1054c0
David Kim <davskim@umich.edu>
2022-12-12 14:12:02 -0500

    David's Changes
    Added in Pie chart functionality (hard coded some things, but I will work on undoing that)

--- JoshHCSAnalysis/PruneHCSResults/importHCSresult.m
+++ JoshHCSAnalysis/PruneHCSResults/importHCSresult.m
@@ -1,4 +1,4 @@
-function [sessionResults, accessoryResults] = importHCSresult
+%function [sessionResults, accessoryResults] = importHCSresult
 %8.10.22, ask for input about where session document is/are
 %Gets HCS session analysis results.
 %Returns accessoryResults cell array with other data (name of experiement, total time doing behavior x)
@@ -6,7 +6,10 @@ function [sessionResults, accessoryResults] = importHCSresult
 %Rows 10 and 11 in accessoryResults aren't important and might just need to
 %get deleted.
 
-fileName = uigetfile('*.csv');
+if ~exist("fileName",'var')
+    fileName = uigetfile('*.csv');
+end
+
 tempSessionResults = read_mixed_csv_HCS(fileName);
 numColumns = size(tempSessionResults, 2); 
 
@@ -18,4 +21,38 @@ numRowsResults = size(tempSessionResults, 1) - 57;                              
 accessoryResults(12:end, 1:end) = tempSessionResults(( (numRowsResults+1) + 11):end, 1:end);%copy bottom portion of tempSessionResults 
 
 sessionResults = tempSessionResults(12:(11+numRowsResults), 1:end);
+
+%% Baking some pie charts
+behavcol = sessionResults(:,4);
+uniqueBehavs = unique(behavcol);
+cop = uniqueBehavs; %It's used somewhere down there... I can take some time to make this a hair more efficient, but i'm lazy, and my phd apps are due this week, so I guess I'm just gonna go fuck myself...
+framecounts = sessionResults(:,3);
+framecounts = ConvertEveryGoddamnedStringInsideThisGoddamnedCellIntoaMotherfuckingInteger(framecounts);
+
+for i = 1:length(uniqueBehavs)
+    indicesofbehavs = strcmp(behavcol,uniqueBehavs{i});
+    uniqueBehavs{i,2} = sum(framecounts(indicesofbehavs));
+end
+
+[sortedFramecounts,permutatrix] = sort(cell2mat(uniqueBehavs(:,2)),'descend');
+sortedBehavs = cop(permutatrix);
+
+figure;
+FramecountPercentages = sortedFramecounts / sum(sortedFramecounts);
+pie(FramecountPercentages(FramecountPercentages > .01))%, sortedBehavs(FramecountPercentages > .01));
+
+%end
+
+
+
+function [numbers] = ConvertEveryGoddamnedStringInsideThisGoddamnedCellIntoaMotherfuckingInteger(allStrings)
+    % Holy shit...
+    numbers = zeros(length(allStrings),1);
+    for i = 1:length(allStrings)
+        numbers(i) = str2num(allStrings{i});
+    end
+end
+
+function [bigassBehavs, bigassFramecounts] = CombineHCSData()
+    bigass = [hana(:); dul(:)];
 end
